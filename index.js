ccb0192bf81ceea8bcc341ee4ed1e65d41fdf380
jovanigomez <jovani.gomez@gmail.com>
2022-09-07 23:32:37 -0700

    questions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
-
+import inquirer from "inquirer";
+import * as fs from "fs";
+import path from "path";
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -8,6 +10,9 @@ function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
+ inquirer
+ .prompt([
+     {type: 'List' 'license', message: 'choose your license', choices: ['GPL 3.0', 'GNU', 'MIT', APACHE 2.0]}
 
 // Function call to initialize app
 init();
