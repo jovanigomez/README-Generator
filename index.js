692cfb986b13689fc8f24019cb36467b9218cd01
jovanigomez <jovani.gomez@gmail.com>
2022-09-08 22:38:10 -0700

    questions

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ function writeToFile(fileName, data) {}
 function init() {}
  inquirer
  .prompt([
-     {type: 'list', name: 'license', message: 'choose your license', choices: ['GPL 3.0', 'GNU', 'MIT', APACHE 2.0]},
+     {type: 'list', name: 'license', message: 'choose your license', choices: ['GPL 3.0', 'GNU', 'MIT', 'APACHE 2.0']},
      {type: 'input', name: 'email', message: 'input email address.'},
      {type: 'input', name: 'project', message: 'whats the name of your project?'},
      {type: 'input', name: 'discription', message: 'discribe your project.'},
      {type: 'input', name: 'github', message: 'what is your username?'},
-     {type: 'input', name: 'install', message: 'how do you install your project?'}
+     {type: 'input', name: 'install', message: 'how do you install your project?'},
      {type: 'input', name: 'test', message: 'which command do you use to run it?'},
-     {type: 'input', name: 'contributers', message: 'how should users contribute?'},
-// Function call to initialize app
+     {type: 'input', name: 'contributers', message: 'how should users contribute?'}
+ ])
+
+ // Function call to initialize app
 init();
 
